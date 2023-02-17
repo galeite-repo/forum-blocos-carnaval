a51e171d742750e4d8b082e281453b6a407cd7cf
Matheus Guimarães <br.matheusguimaraes@gmail.com>
2023-02-17 15:49:56 -0300

    Alteração Header
    Alteração Header, ficando apenas uma capa.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,31 +3,23 @@ import { RiLinkedinFill, RiWhatsappFill } from 'react-icons/ri'
 import { AiFillInstagram, AiOutlineArrowDown } from 'react-icons/ai'
 import useSanityImage from 'src/hooks/useSanityImage'
 import Image from 'next/legacy/image'
+
 const Header = ({ header, social }) => {
     const { image, name, ocupation, summary } = header
     const { linkedin, instagram, whatsapp } = social
 
     const imageUrl = useSanityImage();
 
-
     return (
         <header className={styles['header-wrapper']} >
 
             <div className={styles.header}>
 
-
                 {/* HOME IMAGE */}
-                <div className={styles.image}>
-                    <Image src={imageUrl(image).url()} layout='fill' />
+                <div className={styles.imageCapa}>
+                    <img src={imageUrl(header[0].image).url()} alt="Capa Carnaval" className={styles.bgSlide} />
                 </div>
-
-                {/* CONTENT PAGE */}
-                <div className={styles.content}>
-                    <span>{name}</span>
-                    <h1>{ocupation}</h1>
-                    <p>{summary}</p>
-                </div>
-
+                
                 {/* SOCIAL BUTTONS */}
                 <div className={styles.social}>
                     <a href={linkedin}><RiLinkedinFill className={styles.icon} /></a>
@@ -35,9 +27,9 @@ const Header = ({ header, social }) => {
                     <a href={whatsapp}><RiWhatsappFill className={styles.icon} /></a>
                 </div>
 
-                <a href="#about" className={styles.arrow}>
+                {/* <a href="#about" className={styles.arrow}>
                     <AiOutlineArrowDown className={styles.icon} />
-                </a>
+                </a> */}
 
             </div>
         </header>
