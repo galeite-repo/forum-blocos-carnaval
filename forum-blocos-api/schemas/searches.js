dc6399a926ae03a6dec54a5ce5b5ef98f25692a7
Matheus Guimarães <br.matheusguimaraes@gmail.com>
2023-02-17 15:48:01 -0300

    Schema API Pesquisas
    Schema API Pesquisas

---
+++ forum-blocos-api/schemas/searches.js
@@ -0,0 +1,11 @@
+export default {
+    name: 'searches',
+    title : 'Searches',
+    type: 'document',
+    fields: [
+        {name: 'title', title:'Title',type:'string'},
+        {name: 'date', title:'Date',type:'string'},
+        {name: 'button', title:'Button',type:'string'},
+        {name: 'url', title:'URL',type:'string'}
+    ]
+}
