427f194d391fd6ad517bf785280511fc4a06ac79
Matheus Guimarães <br.matheusguimaraes@gmail.com>
2023-02-17 15:50:39 -0300

    Componente Searches e CSS
    Componente Searches e CSS

---
+++ src/Components/Searches/Searches.jsx
@@ -0,0 +1,26 @@
+import styles from './Searches.module.scss'
+
+
+const Searches = ({searches})=>{
+    return(
+        <section className={styles.searchesWrapper} id="searches">
+            <h2>Pesquisas</h2>
+            <div className={styles.searches}>
+                <div className={styles.firstSearch}>
+                    <h4>{searches[0].title}</h4>
+                    <span>{searches[0].date}</span>
+                    <a href={searches[0].url}>{searches[0].button}</a>
+                </div>
+                <div className={styles.secondSearch}>
+                    <h4>{searches[1].title}</h4>
+                    <span>{searches[1].date}</span>
+                    <a href={searches[1].url}>{searches[1].button}</a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+
+export default Searches;
